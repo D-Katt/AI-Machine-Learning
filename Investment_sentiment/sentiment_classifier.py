0a481e1681e702e8527bcf6753494a6bf8c3c1d6
D-Katt <58660996+D-Katt@users.noreply.github.com>
2021-04-02 17:13:22 +0300

    Retrained TF Hub model added

--- Investment_sentiment/sentiment_classifier.py
+++ Investment_sentiment/sentiment_classifier.py
@@ -1,7 +1,8 @@
-"""Transfer learning is used to create a classification model
-for identifying financial news sentiment from the retail investor perspective.
-TF Hub pre-trained model is used as a basis with additional layers for 3 classes.
-Original token based text embedding model was trained on English Google News 130GB corpus.
+"""Пример transfer learning: настройка готовой модели с TF Hub
+для классификации тональности финансовых новостей с точки зрения частных инвесторов.
+К готовой модели добавляются новые слои для маркирования текстов по 3 классам:
+негативные, нейтральные и позитивные новости.
+Исходная модель обучалась на основе датасета English Google News (130 Гб).
 """
 
 import tensorflow_hub as hub
@@ -17,30 +18,29 @@ from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# DataFrame display settings
+# Параметры отображения таблиц
 pd.set_option('display.max_colwidth', 250)
 
-# Charts display settings
+# Параметры отображения графиков
 plt.style.use('fivethirtyeight')
 plt.rcParams['figure.figsize'] = 9, 6
 plt.rcParams.update({'font.size': 11})
 
-# Variable defines if classes in the original dataset will be equally balanced.
+# Переменная определяет, будут ли классы балансироваться перед обучением
 BALANCED = True
 
-# Original data: 4,846 text samples with labelled categories
+# Исходные данные: 4,846 образцов текста с размеченными категориями
 data = pd.read_csv('all_data.csv',
                    header=None,
                    names=['sentiment', 'text'],
                    encoding='latin-1',
                    dtype={'sentiment': 'category'})
 
-# ---------------------- EDA and text preprocessing ------------------------
+# ---------------------- EDA и обработка текста ------------------------
 
-# Balance of classes
+# Соотношение классов
 classes_distribution = data['sentiment'].value_counts()
 
-# Class distribution plot
 labels = classes_distribution.index
 values = classes_distribution.values
 plt.pie(values, labels=labels, startangle=90, autopct='%1.1f%%')
@@ -48,15 +48,15 @@ plt.title('Distribution of Classes')
 plt.tight_layout()
 plt.show()
 
-# Pre-trained model takes a batch of sentences in a 1-D tensor of strings as input
-# and preprocesses it by splitting text samples on spaces.
-# Punctuation removal and other text cleaning operations are not included.
+# Готовая модель принимает на вход пакет текстов как одномерный тенсор
+# и обрабатывает его, разбивая текст по пробелам.
+# Удаление пунктуации и другие операции по "очистке" текста в модели не производятся.
 
 
 def text_cleaning(s: str):
-    """Function removes punctuation and doubles spaces
-    from the text string, removes any words that contain
-    less that 2 characters and converts the string to lowercase."""
+    """Функция удаляет знаки препинания и двойные пробелы,
+    слова, содержащие менее 2 символов, преобразует текст
+    в нижний регистр."""
     s = s.translate(str.maketrans('', '', string.punctuation))
     s = s.replace('  ', ' ').lower()
     s = ' '.join([word for word in s.split() if (len(word) > 1 and word.isalpha())])
@@ -65,7 +65,7 @@ def text_cleaning(s: str):
 
 data['text_cleaned'] = data['text'].apply(text_cleaning)
 
-# Text length statistics
+# Длина текстов
 data['n_words'] = data['text_cleaned'].apply(lambda x: len(x.split()))
 
 min_length = data['n_words'].min()
@@ -76,7 +76,6 @@ median_length = data['n_words'].median()
 print(f'Sentence length: {min_length} - {max_length} words\nMean length = '
       f'{mean_length}\nMedian length = {median_length}')
 
-# Text length distribution
 plt.hist(data['n_words'], bins=10)
 plt.axvline(mean_length, color='red', label='Mean')
 plt.axvline(median_length, color='green', label='Median')
@@ -87,55 +86,55 @@ plt.ylabel('Frequency')
 plt.show()
 
 if BALANCED:
-    # Number of samples in the least represented class
+    # Количество текстов в наиболее распространном классе
     quota = data['sentiment'].value_counts().min()
-    # New DataFrame to add equal number of samples from each class
+    # Новый DataFrame для добавления одинакового количества текстов из каждого класса
     balanced_data = pd.DataFrame(columns=['sentiment', 'text_cleaned'])
-    # Reduce each group to the chosen number of samples
+    # Уменьшаем количество текстов в каждом классе до выбранного лимита
     data_groups = data.groupby('sentiment')
     for group in data_groups.indices:
         reduced_class = data_groups.get_group(group)[['sentiment', 'text_cleaned']].iloc[:quota, :]
         balanced_data = balanced_data.append(reduced_class, ignore_index=True)
-    # Update the original DataFrame
+    # Обновляем исходный DataFrame
     data = balanced_data
     del balanced_data
 
-# Input data for the model as a plain text
+# Входные данные для модели в качестве предварительно очищенных фрагментов текста
 X = data['text_cleaned']
 
-# Categories for classification
+# Категории
 class_names = ['negative', 'neutral', 'positive']
 
-# Binary columns for each category
+# Бинарные столбцы для каждой категории
 for category in class_names:
     data[category] = data['sentiment'].apply(lambda x: 1 if x == category else 0)
 
-# Target values for the model
+# Прогнозируемые значения для модели
 y = data[class_names].values
 
-# Withhold 20% of the original data for test purposes.
-# Take into account class imbalances during the split stratifying the data according to y labels.
+# Оставляем 20% исходных данных для тестирования.
+# Принимаем во внимание дисбаланс классов при разбиении на учебную и тестовую выборки.
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=1)
 
-# Convert target values into tensors
+# Преобразуем данные в тенсоры
 y_train = tf.convert_to_tensor(y_train)
 y_test = tf.convert_to_tensor(y_test)
 
-# ------------------------ Loading and modifying the model --------------------------
+# ------------------------ Загрузка и модификация модели --------------------------
 
-# Text embedding is based on Swivel co-occurrence matrix factorization with pre-built OOV.
-# The model maps from text to 20-dimensional embedding vectors.
-# Vocabulary contains 20,000 tokens and 1 out of vocabulary bucket for unknown tokens.
+# При обработке текста модели использует "Swivel co-occurrence matrix" и встроенные символы OOV
+# (Out of Vocabulary). Текст преобразуется в 20-мерный вектор.
+# Словарь содержит 20,000 токенов и 1 символ для не известных слов.
 
-# Load the model from TF Hub
+# Загрузка модели с TF Hub
 hub_layer = hub.KerasLayer('https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim-with-oov/1',
                            output_shape=[20],
                            input_shape=[],
                            dtype=tf.string,
                            trainable=True)
 
-# Sequential model with additional layers.
-# L2 regularization is added to decrease overfitting on a small dataset.
+# Sequential model с дополнительными слоями.
+# Для избежания переобучения на маленьком датасете добавлена регуляризация.
 model = tf.keras.Sequential(
     [
         hub_layer,  # Original model
@@ -149,28 +148,27 @@ model = tf.keras.Sequential(
 
 model.summary()
 
-# Metrics and optimizer
+# Метрики и оптимизатор
 model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               metrics=['accuracy'])
 
 EPOCHS = 500
 
-# Training will stop if no improvement is seen for 10 epochs
+# Обучение будет остановлено при отсутствии прогресса в течение 10 эпох
 early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy',
                                               patience=10,
                                               restore_best_weights=True)
 
-# Dictionary for class_weight parameter of the model
-# to compensate for class imbalance.
+# Словарь весов классов
 if BALANCED:
-    balancing_dict = {index: 1/3 for index in range(3)}
-# If we use original data with imbalanced classes,
-# assign higher weights to less represented categories.
+    balancing_dict = {index: 1 / 3 for index in range(3)}
+# Если используются исходные данные с дисбалансом классов,
+# придаем больший вес наименее представленным категориям.
 else:
     balancing_dict = {0: 0.50, 1: 0.15, 2: 0.35}
 
-# Train the model using 20% of the training data for validation
+# Обучение модели (20% данных используются для валидации)
 history = model.fit(X_train, y_train,
                     batch_size=32, validation_batch_size=32,
                     validation_split=0.2, shuffle=True,
@@ -178,7 +176,7 @@ history = model.fit(X_train, y_train,
                     callbacks=[early_stop],
                     epochs=EPOCHS, verbose=2)
 
-# Evaluate training and validation progress
+# Визуализация метрик в процессе обучения и валидации
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 loss = history.history['loss']
@@ -202,7 +200,7 @@ plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
 
-# Evaluate the model on the test set
+# Оценка модели на тестовых данных
 loss, acc = model.evaluate(X_test, y_test)
 print(f'\nTest loss = {loss}\nTest accuracy = {acc}\n')
 
@@ -210,7 +208,7 @@ actual_labels = np.argmax(y_test, axis=1)
 prediction = np.argmax(model.predict(X_test), axis=1)
 print(classification_report(actual_labels, prediction))
 
-# Confusion matrix for the test set
+# Матрица ошибок для тестовых данных
 cm = confusion_matrix(actual_labels, prediction)
 columns = ['pred_' + name for name in class_names]
 indexes = ['actual_' + name for name in class_names]
@@ -222,7 +220,7 @@ plt.title('Confusion Matrix')
 plt.tight_layout()
 plt.show()
 
-# Results for training on a balanced dataset:
+# Результаты обучения модели на сбалансированных по классам данных:
 
 # Test loss = 0.7876015901565552
 # Test accuracy = 0.7658402323722839
